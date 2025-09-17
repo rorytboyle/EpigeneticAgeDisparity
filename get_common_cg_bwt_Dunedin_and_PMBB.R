c589c16b84ae378eedff8411413bb87f0ae723c7
Hao Xu <74469369+HildaXu@users.noreply.github.com>
2025-09-17 13:59:17 -0400

    Add files via upload

---
+++ get_common_cg_bwt_Dunedin_and_PMBB.R
@@ -0,0 +1,32 @@
+library(readxl)
+library(sesame)
+sesameDataCache()
+library(BiocParallel)
+library(dplyr)
+library(Rtsne)
+library(ggplot2)
+library(viridis)
+library(parallel)
+library(BiocParallel)
+library(writexl)
+
+
+betas <- readRDS("20250529_raw_betas_848.rds")
+
+rownames(betas) <- sub("_.*", "", rownames(betas))
+pace=readRDS("/home/xuh6/20240920_Hao/projects/PMBB_data_analysis/20250212_cpg_DunedinPACE.rds")
+pace_all=readRDS("/home/xuh6/20240920_Hao/projects/PMBB_data_analysis/20250212_cpg_DunedinPACE_all.rds")
+common_probes <- intersect(rownames(betas), pace)
+length(common_probes)  # Number of common probes
+common_probes_all <- intersect(rownames(betas), pace_all)
+length(common_probes_all)  # Number of common probes in the all set
+# save common_probes to a rds file
+saveRDS(common_probes, "20250529_cpg_DunedinPACE_intersect_MSA.rds")
+saveRDS(common_probes_all, "20250529_cpg_DunedinPACE_all_intersect_MSA.rds")
+common_probes <- readRDS("20250529_cpg_DunedinPACE_intersect_MSA.rds")
+length(common_probes)  # Number of common probes
+head(common_probes)  # Check the first few common probes
+common_probes_all <- readRDS("20250529_cpg_DunedinPACE_all_intersect_MSA.rds")
+length(common_probes_all)  # Number of common probes in the all set
+head(common_probes_all)  # Check the first few common probes in the all set
+
